14f4b21f56cef42d4634514a2146c9120d575a46
Jacoby, Thomas C. (MPC) <tjacoby@marathonpetroleum.com>
2018-06-21 08:23:26 -0400

    A little more likely scenario

--- RepositoryPattern/Program.cs
+++ RepositoryPattern/Program.cs
@@ -1,10 +1,12 @@
-using RepositoryPattern.Entities;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Data.Entity;
+using RepositoryPattern.Models;
+using System.Data.SqlClient;
+using System.Data;
 
 namespace RepositoryPattern
 {
@@ -12,22 +14,63 @@ namespace RepositoryPattern
     {
         static void Main(string[] args)
         {
-            var ctx = new Ctx();
-            var productIds = new[] { 1, 41, 64 };
-            var products = ctx.Products
-                                .Include(product => product.Attributes)
-                                .Where(product => productIds.Contains(product.ID))
-                                .ToList();
-            var customer = new Customer { Name = "John Smith" };
-            ctx.Customers.Add(customer);
+            var transactions = GetTransactionsFromFile();
+            Console.WriteLine($"Found {transactions.Count()} transactions.");
 
-            var order = new Order();
-            var orderItems = products.Select(product => new OrderProductUnits { Product = product, Units = 4 }).ToList();
-            foreach (var item in orderItems)
+            var specificTransaction = transactions.FirstOrDefault(x => x.RecordId == "TheOne");
+            if (specificTransaction != null)
+                Console.WriteLine($"Found the {specificTransaction.RecordId} transactions.");
+            else
+                Console.WriteLine("Couldn't find that one transaction we were looking for. Bummer.");
+        }
+
+        private static IEnumerable<Transaction> GetTransactionsFromFile()
+        {
+            var fileLines = System.IO.File.ReadAllLines(@"c:\temp\transaction-data.txt");
+            var transactions = fileLines.Select(line => line.Split('~'))
+                                        .Select(values => new Transaction
+                                        {
+                                            RecordId = values[0],
+                                            Customer = values[1],
+                                            Location = values[2],
+                                            Product = values[3],
+                                            Timestamp = DateTime.TryParse(values[4], out var datetimeValue) ? datetimeValue : DateTime.MinValue,
+                                            Volume = double.TryParse(values[5], out var doubleValue) ? doubleValue : 0
+                                        }).ToList();
+
+            return transactions;
+        }
+
+        private static IEnumerable<Transaction> GetTransactionsFromDatabase()
+        {
+            DataTable results = null;
+            using (var conn = new SqlConnection("my database connection"))
             {
-                order.Items.Add(item);
+                using (var cmd = new SqlCommand("select * from MainframeTransactions", conn))
+                {
+                    using (var reader = cmd.ExecuteReader())
+                    {
+                        while (!reader.IsClosed)
+                        {
+                            results = new DataTable();
+                            results.Load(reader);
+                        }
+                    }
+                }
             }
-            
+
+            var transactions = results.AsEnumerable()
+                                    .Select(row => new Transaction
+                                    {
+                                        RecordId = row[0].ToString(),
+                                        Customer = row[1].ToString(),
+                                        Location = row[2].ToString(),
+                                        Product = row[3].ToString(),
+                                        Timestamp = DateTime.TryParse(row[4].ToString(), out var datetimeValue) ? datetimeValue : DateTime.MinValue,
+                                        Volume = double.TryParse(row[5].ToString(), out var doubleValue) ? doubleValue : 0
+                                    });
+
+            return transactions;
         }
     }
 }
