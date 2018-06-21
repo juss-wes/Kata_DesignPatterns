244902218b5ff6290e5f1b74a1ba8fb0f1872cec
Jacoby, Thomas C. (MPC) <tjacoby@marathonpetroleum.com>
2018-06-21 10:59:35 -0400

    Rename variables, remove random instance.

--- FactoryPattern/Program.cs
+++ FactoryPattern/Program.cs
@@ -13,35 +13,17 @@ namespace FactoryPattern
         {
             var color = "Red";
 
-            var camry = new ToyotaCamry();
-            camry.Color = color;
-            camry.Drive();
+            var dailyDriver = new ToyotaCamry();
+            dailyDriver.Color = color;
+            dailyDriver.Drive();
 
-            var viper = new DodgeViper();
-            viper.Color = color;
-            viper.Drive();
+            var sportsCar = new DodgeViper();
+            sportsCar.Color = color;
+            sportsCar.Drive();
 
-            var superLow = new HarleyDavidsonSuperLow();
-            superLow.Color = color;
-            superLow.Drive();
-
-            Automobile randomAuto;
-            switch (new Random(DateTime.Now.Millisecond).Next(0, 2))
-            {
-                case 0:
-                    randomAuto = new ToyotaCamry();
-                    break;
-                case 1:
-                    randomAuto = new DodgeViper();
-                    break;
-                case 2:
-                    randomAuto = new HarleyDavidsonSuperLow();
-                    break;
-                default:
-                    throw new Exception("Unexpected random number.");
-            }
-            randomAuto.Color = color;
-            randomAuto.Drive();
+            var motorcycle = new HarleyDavidsonSuperLow();
+            motorcycle.Color = color;
+            motorcycle.Drive();
         }
     }
 }
