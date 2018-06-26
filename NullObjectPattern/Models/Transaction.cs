00a7927b7f59f7ec4effdb7e5191c192d6fdf593
Wesley Vollmar <wcvollmar@marathonpetroleum.com>
2018-06-26 09:16:18 -0400

    Initial start, which checks for null and handles control flow differently

---
+++ NullObjectPattern/Models/Transaction.cs
@@ -0,0 +1,29 @@
+using System;
+
+namespace NullObjectPattern.Models
+{
+    public class Transaction
+    {
+        public int ID { get; set; } = 0;
+        public string Name { get; set; } = string.Empty;
+        public double Cost { get; set; } = 0.00d;
+        public DateTime Date { get; set; } = DateTime.MinValue;
+
+        public Transaction()
+        { }
+
+        public Transaction(int id, string name, double cost, DateTime date)
+        {
+            ID = id;
+            Name = name;
+            Cost = cost;
+            Date = date;
+        }
+
+        public virtual void ProcessTransaction()
+        {
+            // Save/charge transaction using whatever method that might use
+            Console.WriteLine($"Transaction {ID} for {Cost} Processed");
+        }
+    }
+}
